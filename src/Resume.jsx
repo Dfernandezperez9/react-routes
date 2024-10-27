91a30efe9bc5a99e67d967d37a159a8f55b2c225
David <dfernandezperez9@gmail.com>
2024-10-27 18:01:55 +0100

    Completed

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -1,19 +1,3 @@
-/*import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-
-function Resume() {
-    return (
-        <>
-            <h1>Resume</h1>
-            <p>This is the resume page</p>
-
-            <Link to="/">Home</Link>
-        </>
-    );
-}
-
-export default Resume;*/
-
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { studies, experiences } from "./data/resume";
